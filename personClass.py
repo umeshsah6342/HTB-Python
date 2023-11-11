49afed3c3e6271169be83879c462158c959dc0bc
umeshsah6342 <umeshsah6342@gmail.com>
2023-11-11 13:52:47 -0500

    Added an object method

--- personClass.py
+++ personClass.py
@@ -2,7 +2,10 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+    def myfunc(self):
+        print(f"The First Person name is {self.name} having age {self.age}")
 
 p1 = Person("John", 36)
 print(p1.name)
 print(p1.age)
+p1.myfunc()
